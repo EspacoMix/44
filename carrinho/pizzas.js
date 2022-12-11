8c04f38f21afce0ead1018a49dbb51b8f482b0c9
TodosJuntosJAA <105557405+TodosJuntosJAA@users.noreply.github.com>
2022-12-11 13:55:14 -0300

    Add files via upload

---
+++ carrinho/pizzas.js
@@ -0,0 +1,50 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Aluguel do Espaço',
+        img: '../img/epaco_5.jpg',
+        price: [380.00, 380.00, 380.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: ' '
+      },
+      {
+        id: 2,
+        name: 'Pula-Pula',
+        img: 'images/pizza2.png',
+        price: [0, 0, 100.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: ''
+      },
+      {
+        id: 3,
+        name: 'Pis.bolinhas',
+        img: 'images/pizza3.png',
+        price: [ ,  , 100.00],
+        sizes: [
+          ' ',
+          ' ',
+          ' '
+        ],
+        description: ' '
+      },
+      {
+        id: 4,
+        name: 'Bar.Pipoca',
+        img: 'images/pizza4.png',
+        price: [ ,  , 100.00],
+        sizes: [
+          ' ',
+          ' ',
+          ' '
+        ],
+        description: ' '
+      }
+];
